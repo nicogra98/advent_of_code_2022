5bf90204a4d9fe2cde31043dbd304a1f11d374a4
Nicolas Granados <nicogranados1210@gmail.com>
2022-12-05 23:08:53 +0100

    day 1 part 2

--- day1/main.py
+++ day1/main.py
@@ -12,8 +12,22 @@ def part_1(calories):
             currentSum = 0
     return maxSum
 
+def part_2(calories):
+    elves = []
+    currentSum = 0
+    for calorie in calories:
+        if calorie != "":
+            currentSum += int(calorie)
+        else:
+            elves.append(currentSum)
+            currentSum = 0
+    elves.append(currentSum)
+    return sum(sorted(elves,reverse=True)[:3])
+
+
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
         calories = f.read().split("\n")
         
         print(part_1(calories))
+        print(part_2(calories))
