b0be197a2bdfa9ecf0a5b89d3dbae86f84094cbc
Nicolas Granados <nicogranados1210@gmail.com>
2022-12-08 11:09:08 +0100

    day 3 part 2

--- day3/main.py
+++ day3/main.py
@@ -6,7 +6,6 @@ def part_1 (rucksacks):
 
         for letter in top:
             if letter in bottom:
-                print(letter)
                 if ord(letter) >= 97:
                     priority += (ord(letter) - ord("a") + 1)
                     break
@@ -14,11 +13,27 @@ def part_1 (rucksacks):
                     priority += (ord(letter) - ord("A") + 27)
                     break
     return priority
+    
+def part_2(rucksacks):
+    priority = 0
+
+    for i in range(0, len(rucksacks), 3):
+        group = rucksacks[i:i+3]
+        common = set(group[0]).intersection(set(group[1])).intersection(set(group[2]))
+        letter = next(iter(common))
+
+        if ord(letter) >= 97:
+            priority += (ord(letter) - ord("a") + 1)
+        else:
+            priority += (ord(letter) - ord("A") + 27)
+
+    return priority
+
 
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
         rucksacks = f.read().split("\n")
 
-        print(ord("b") - ord("a") + 1)
         print(part_1(rucksacks))
+        print(part_2(rucksacks))
